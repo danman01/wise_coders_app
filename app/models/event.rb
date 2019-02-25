c35a634ac9e4a4bb8799265976751f08cb791071
Natesuri <natesuri@gmail.com>
2019-02-25 13:02:05 -0500

    Fixes interests and events models
    Revises the interest model to declare the has_many: :user_interests
    before the has_many: :users, though: :user_interests
    
    Revises the event model to declare the has_many: :user_events
    before the has_many: :users, though: :user_events
    
    Need to declare the join table before the resource to work properly.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  has_many :users, through: :user_events
   has_many :user_events, dependent: :destroy
+  has_many :users, through: :user_events
   has_many :event_happenings, dependent: :destroy
   has_many :happenings, through: :event_happenings
   has_many :event_repeats, dependent: :destroy
