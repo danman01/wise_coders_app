bff095b070c5855725eba5cfe5b691a97d4d0f6b
Aimee <aimeelynnramirez@gmail.com>
2019-03-02 14:04:00 -0500

    Delete devise.rb

--- config/initializers/devise.rb
+++
@@ -1,4 +0,0 @@
-Devise.setup do |config|
-  #config.secret_key = ENV['secret_key_base']
-   config.secret_key = '4ef019d3073db55e9108c114b1506fff4439915ae30225e2c251271ff8b6341ee5d00fb779c6a8ddc520c863ed33ef7877177b3f1e5e69bf9c65330f84f0fcff'
-end
